a8e9a11153652cd4b167815eb82817c38d3f1b97
engenegr <evdokimov.ilya@gmail.com>
2020-12-28 18:29:11 +0100

    Revert "onion update"
    This reverts commit 1836b093

--- app/src/main/java/com/lightning/walletapp/ln/crypto/Sphinx.scala
+++ app/src/main/java/com/lightning/walletapp/ln/crypto/Sphinx.scala
@@ -27,6 +27,7 @@ import fr.acinq.bitcoin.{Crypto, Protocol}
 import scodec.bits.ByteVector
 import scodec.Attempt
 
+
 object Sphinx {
   type PublicKeyVec = Vector[PublicKey]
 
@@ -305,8 +306,6 @@ object Sphinx {
     def create(sharedSecret: ByteVector, failure: FailureMessage): ByteVector = {
       val um = generateKey("um", sharedSecret)
       val packet = FailureMessageCodecs.failureOnionCodec(Hmac256(um)).encode(failure).require.toByteVector
-      println(s"um key: $um")
-      println(s"raw error packet: ${packet.toHex}")
       wrap(packet, sharedSecret)
     }
 
